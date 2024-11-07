95ac2e5f061f151118dbb618d4b184e086a05cd0
LiWeiJie001 <3258926778@qq.com>
2024-11-07 19:02:13 +0800

    修改了轮播图的速度

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ var swiper = new Swiper('.swiper-container', {
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
