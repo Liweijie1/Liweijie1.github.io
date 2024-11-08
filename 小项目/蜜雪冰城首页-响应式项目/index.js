955f0e5256a2a4491ff97797553a4bd494d15f48
LiWeiJie001 <3258926778@qq.com>
2024-11-08 10:02:27 +0800

    2024.11.8更新了字体,添加了鼠标悬浮样式

---
+++ 小项目/蜜雪冰城首页-响应式项目/index.js
@@ -0,0 +1,22 @@
+// 二级列表上拉下拉特效
+$(".father-list li").mouseenter(function(){
+  $(this).children(".child-list").stop().slideDown(); // 显示子列表，上拉效果
+}).mouseleave(function(){
+  $(this).children(".child-list").stop().slideUp(); // 隐藏子列表，下拉效果
+});
+
+// 三个产品 swiper轮播图插件
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
